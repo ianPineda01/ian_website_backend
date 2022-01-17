55f4706de1e039d09a48e763f11df573250fd172
ianPineda <ianpineda@ciencias.unam.mx>
2022-01-17 10:49:36 -0600

    Add vue frontend and update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const port = 3000;
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
 });
 
 app.get('/', (req, res) => {
-    res.send("Welcome to my server :D");
+    res.sendFile(path.join(__dirname, 'dist', 'dist.html'))
 });
 
-app.get("/:name", (req, res) => {
-    res.send(`Hello ${req.params.name}`);
+app.post("/:name", (req, res) => {
+    res.json({ username: req.params.name});
 });
