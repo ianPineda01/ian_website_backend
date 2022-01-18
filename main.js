e95429b725be914f91b3ae2ed6f3f3ad5c04779d
ianPineda <ianpineda@ciencias.unam.mx>
2022-01-18 17:22:26 -0600

    Add visit counter

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const history = require('connect-history-api-fallback');
+const fs = require('fs/promises');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(history({index: '/'}));
 
 app.listen(port, () => {
-    console.log(`App listening at port ${port}`);
+    console.log(`App running at http://localhost:${port}/`);
 });
 
 app.get('/', (req, res) => {
@@ -17,4 +18,14 @@ app.get('/', (req, res) => {
 
 app.post("/", (req, res) => {
     res.json({ message: 'welcome to my server :)'});
+});
+
+app.post("/log_visit", () => {
+    fs.readFile('visitor_count.txt', 'utf-8')
+    .then(data => parseInt(data))
+    .then(data => data + 1)
+    .then(data => '' + data)
+    .then(data => {
+        fs.writeFile('visitor_count.txt', data);
+    });
 });
